bf27888bcea3824b9f49ce066b9e354df3b6ecfe
DarioConiglione <dariocng13@gmail.com>
2025-11-17 16:43:43 +0100

    Start project with react route

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path element></Route>
+          <Route path element></Route>
+          <Route path element></Route>
+        </Routes>
+
+
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
