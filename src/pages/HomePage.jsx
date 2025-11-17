ba898a10b252b3bf7250e7aab4f3eab8e2ff5300
DarioConiglione <dariocng13@gmail.com>
2025-11-17 17:37:00 +0100

    add main

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,7 +37,24 @@ export default function HomePage() {
                 </nav>
             </header>
 
-            <main></main>
+            <main>
+                <div className="p-5 mb-4 bg-secondary-subtle rounded-3">
+                    <div className="container-fluid py-5">
+                        <h1 className="display-5 fw-bold">Benvenuti nel progetto DEMO!</h1>
+                        <p className="col-md-8 fs-4">
+                            Using a series of utilities, you can create this jumbotron, just
+                            like the one in previous versions of Bootstrap. Check out the
+                            examples below for how you can remix and restyle it to your liking.
+                        </p>
+                        <button className="btn btn-primary btn-lg" type="button">
+                            Example button
+                        </button>
+                    </div>
+                </div>
+                <div className="container bg-light p-3 rounded-3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur totam dicta soluta at, perferendis ipsa officiis possimus iste dolor sequi debitis earum minus ea dolorum necessitatibus placeat iure obcaecati atque neque quia culpa magnam unde cum! Natus adipisci cupiditate sequi ducimus, facere earum fugiat porro aut repellendus beatae culpa libero ipsa minima nostrum illo molestias modi! Nemo qui blanditiis asperiores illum voluptatum culpa magni fugiat praesentium, veritatis sint rerum quaerat aspernatur temporibus, eveniet dolorum consequuntur autem iure exercitationem obcaecati harum tempore beatae quisquam nam. Laudantium autem voluptatem omnis, quia quidem architecto! Ipsa doloremque maxime neque est? Consectetur ab suscipit, at quidem esse obcaecati. Ullam illum cupiditate sed ut dolorem molestias fuga soluta consequatur quidem, voluptatum quia quae voluptatem suscipit pariatur facere, dolores vel, explicabo dignissimos! At dolorum blanditiis architecto doloribus repellat eius reprehenderit voluptatum nobis deserunt possimus accusamus, a dignissimos? Facilis sapiente iure totam maiores quae unde architecto, placeat maxime repudiandae qui voluptatibus cupiditate nobis porro quam mollitia accusantium obcaecati illum incidunt magnam. Aperiam molestiae omnis vero autem, est voluptas sit amet explicabo molestias quia a eius consequuntur reprehenderit! Ab non voluptatem cupiditate voluptas nam omnis eum earum! Quibusdam necessitatibus est suscipit fugiat debitis, impedit dolor? Rerum corporis totam pariatur!</p>
+                </div>
+            </main>
             <footer></footer>
 
 
